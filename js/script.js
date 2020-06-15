e3f98252a159475a636d9fe43be5b5a0b7abc64c
0mbmb <as.klementev@gmail.com>
2020-06-15 22:54:00 +0300

    Итоговые правки

--- js/script.js
+++ js/script.js
@@ -14,24 +14,114 @@ var deliveryInfo = document.querySelector('.services__info--delivery');
 var warrantyInfo = document.querySelector('.services__info--warranty');
 var creditInfo = document.querySelector('.services__info--credit');
 
+// Contact us
 var contactUsButton = document.querySelector('.information__button--contact-us');
 var contactUsModal = document.querySelector('.contact-us');
-var contactUsForm = contactUsModal.querySelector('.contact-us__form');
-var contactUsModalClose = contactUsModal.querySelector('.modal__close--contact-us');
-var nameContactUs = contactUsModal.querySelector('.contact-us__name input');
-var emailContactUs = contactUsModal.querySelector('.contact-us__email input');
-var commentContactUs = contactUsModal.querySelector('.contact-us__comment textarea');
+if (contactUsModal) {
+  var contactUsForm = contactUsModal.querySelector('.contact-us__form');
+  var contactUsModalClose = contactUsModal.querySelector('.modal__close--contact-us');
+  var nameContactUs = contactUsModal.querySelector('.contact-us__name input');
+  var emailContactUs = contactUsModal.querySelector('.contact-us__email input');
+  var commentContactUs = contactUsModal.querySelector('.contact-us__comment textarea');
 
+  // Contact us modal open
+  contactUsButton.onclick = function (evt) {
+    evt.preventDefault();
+    contactUsModal.classList.remove('visually-hidden');
+    contactUsModal.classList.add('modal-animation');
+
+    if (name && email) {
+      nameContactUs.value = name;
+      emailContactUs.value = email;
+      commentContactUs.focus();
+    } else {
+      nameContactUs.focus();
+    }
+  }
+
+  // Contact us form validation
+  contactUsForm.addEventListener('submit', function(evt) {
+    if (!nameContactUs.value || !emailContactUs.value || !commentContactUs.value) {
+      evt.preventDefault();
+      contactUsModal.classList.remove("modal-error");
+      contactUsModal.offsetWidth = contactUsModal.offsetWidth;
+      contactUsModal.classList.add('modal-error');
+    } else {
+      localStorage.setItem('name', nameContactUs.value);
+      localStorage.setItem('email', emailContactUs.value);
+    }
+  });
+}
+
+// Map
 var mapButton = document.querySelector('.information__map');
 var mapModal = document.querySelector('.map-modal');
-var mapModalClose = mapModal.querySelector('.modal__close--map')
+if (mapModal) {
+  var mapModalClose = mapModal.querySelector('.modal__close--map')
 
-var sliderPrev = document.querySelector('.slider__prev');
-var sliderNext = document.querySelector('.slider__next');
+  // Map modal open
+  mapButton.onclick = function (evt) {
+    evt.preventDefault();
+    mapModal.classList.remove('visually-hidden');
+    mapModal.classList.add('modal-animation');
+    mapModalClose.focus();
+  }
+}
 
-var slidersContainer = document.querySelector('.slider__list');
-var sliderRadio1 = document.querySelector('.slider__radio--1');
-var sliderRadio2 = document.querySelector('.slider__radio--2');
+// Slider
+var slider = document.querySelector('.slider');
+if (slider) {
+  var sliderPrev = slider.querySelector('.slider__prev');
+  var sliderNext = slider.querySelector('.slider__next');
+
+  var slidersContainer = slider.querySelector('.slider__list');
+  var sliderRadio1 = slider.querySelector('.slider__radio--1');
+  var sliderRadio2 = slider.querySelector('.slider__radio--2');
+
+  // Slider controls
+  sliderNext.onclick = function (evt) {
+    evt.preventDefault();
+    slidersContainer.scroll({
+      top: 0,
+      left: 620,
+      behavior: 'smooth'
+    });
+    sliderRadio2.classList.add('slider__radio--active');
+    sliderRadio1.classList.remove('slider__radio--active');
+  }
+
+  sliderPrev.onclick = function (evt) {
+    evt.preventDefault();
+    slidersContainer.scroll({
+      top: 0,
+      left: -620,
+      behavior: 'smooth'
+    });
+    sliderRadio1.classList.add('slider__radio--active');
+    sliderRadio2.classList.remove('slider__radio--active');
+    // sliderRadio1.checked = true;
+  }
+
+  sliderRadio1.onclick = function () {
+    slidersContainer.scroll({
+      top: 0,
+      left: -620,
+      behavior: 'smooth'
+    });
+    sliderRadio1.classList.add('slider__radio--active');
+    sliderRadio2.classList.remove('slider__radio--active');
+  }
+
+  sliderRadio2.onclick = function () {
+    slidersContainer.scroll({
+      top: 0,
+      left: 620,
+      behavior: 'smooth'
+    });
+    sliderRadio2.classList.add('slider__radio--active');
+    sliderRadio1.classList.remove('slider__radio--active');
+  }
+}
 
 var isStorageSupport = true;
 var name = '';
@@ -44,24 +134,31 @@ try {
 }
 
 // Info selector
-for (var selectorButton of selectorButtons) {
+for (let selectorButton of selectorButtons) {
   selectorButton.onclick = function () {
     selectorButton.setAttribute("disabled", true);
+    selectorButton.classList.add("services__selector-button--active");
     if (selectorButton === deliveryButton) {
       warrantyButton.removeAttribute("disabled");
       creditButton.removeAttribute("disabled");
+      warrantyButton.classList.remove("services__selector-button--active");
+      creditButton.classList.remove("services__selector-button--active");
       deliveryInfo.classList.remove('visually-hidden');
       warrantyInfo.classList.add('visually-hidden');
       creditInfo.classList.add('visually-hidden');
     } else if (selectorButton === warrantyButton) {
       deliveryButton.removeAttribute("disabled");
       creditButton.removeAttribute("disabled");
+      deliveryButton.classList.remove("services__selector-button--active");
+      creditButton.classList.remove("services__selector-button--active");
       warrantyInfo.classList.remove('visually-hidden');
       deliveryInfo.classList.add('visually-hidden');
       creditInfo.classList.add('visually-hidden');
     } else {
       deliveryButton.removeAttribute("disabled");
       warrantyButton.removeAttribute("disabled");
+      deliveryButton.classList.remove("services__selector-button--active");
+      warrantyButton.classList.remove("services__selector-button--active");
       creditInfo.classList.remove('visually-hidden');
       warrantyInfo.classList.add('visually-hidden');
       deliveryInfo.classList.add('visually-hidden');
@@ -106,76 +203,4 @@ for (var addToCartButton of allAddToCartButtons) {
 modalContinue.onclick = function (evt) {
   evt.preventDefault();
   cartModal.classList.add('visually-hidden');
-}
-
-// Contact us modal open
-contactUsButton.onclick = function (evt) {
-  evt.preventDefault();
-  contactUsModal.classList.remove('visually-hidden');
-  contactUsModal.classList.add('modal-animation');
-
-  if (name && email) {
-    nameContactUs.value = name;
-    emailContactUs.value = email;
-    commentContactUs.focus();
-  } else {
-    nameContactUs.focus();
-  }
-}
-
-// Contact us form validation
-contactUsForm.addEventListener('submit', function(evt) {
-  if (!nameContactUs.value || !emailContactUs.value || !commentContactUs.value) {
-    evt.preventDefault();
-    contactUsModal.classList.remove("modal-error");
-    contactUsModal.offsetWidth = contactUsModal.offsetWidth;
-    contactUsModal.classList.add('modal-error');
-  } else {
-    localStorage.setItem('name', nameContactUs.value);
-    localStorage.setItem('email', emailContactUs.value);
-  }
-});
-
-// Map modal open
-mapButton.onclick = function (evt) {
-  evt.preventDefault();
-  mapModal.classList.remove('visually-hidden');
-  mapModalClose.focus();
-}
-
-// Slider
-sliderNext.onclick = function (evt) {
-  evt.preventDefault();
-  slidersContainer.scroll({
-    top: 0,
-    left: 620,
-    behavior: 'smooth'
-  });
-  sliderRadio2.checked = true;
-}
-
-sliderPrev.onclick = function (evt) {
-  evt.preventDefault();
-  slidersContainer.scroll({
-    top: 0,
-    left: -620,
-    behavior: 'smooth'
-  });
-  sliderRadio1.checked = true;
-}
-
-sliderRadio1.onclick = function () {
-  slidersContainer.scroll({
-    top: 0,
-    left: -620,
-    behavior: 'smooth'
-  });
-}
-
-sliderRadio2.onclick = function () {
-  slidersContainer.scroll({
-    top: 0,
-    left: 620,
-    behavior: 'smooth'
-  });
 }
